181a3c562a44499fde2cc759cc9786c0bce9d5a5
zuiaizengdada <1203049946@qq.com>
2023-11-19 23:13:34 +0800

    接入富文本编辑器

--- src/index.tsx
+++ src/index.tsx
@@ -12,9 +12,7 @@ import 'normalize.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
